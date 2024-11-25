414a66927ffa0218784bfd427a51804b45bd911c
TheLp281 <efekantunc0@gmail.com>
2024-11-25 03:38:17 +0300

    Implement SSE, remove appmanager and manual action handling, use aspnet default route handling, migrate all classes
    Former-commit-id: 4aa3b2a4d94565aea93edc945d8ec5dc650d8482

---
+++ SSEManager.cs
@@ -0,0 +1,75 @@
+using System.Security.Claims;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using LiventCord.Controllers;
+using LiventCord.Models;
+
+public class SSEManager
+{
+    private readonly Dictionary<string, HttpContext> _activeConnections = new();
+
+    public SSEManager()
+    {
+    }
+
+    public async Task EventsStream(HttpContext context, string guildId)
+    {
+        context.Response.ContentType = "text/event-stream";
+        context.Response.Headers.Add("Cache-Control", "no-cache");
+        context.Response.Headers.Add("Connection", "keep-alive");
+
+        string userId = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (string.IsNullOrEmpty(userId))
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync("User is not authenticated.");
+            return;
+        }
+
+        AddConnection(userId, context);
+        await Task.Delay(Timeout.Infinite);
+    }
+
+    public void AddConnection(string userId, HttpContext context)
+    {
+        if (!_activeConnections.ContainsKey(userId))
+        {
+            _activeConnections.Add(userId, context);
+        }
+        else
+        {
+            _activeConnections[userId] = context;
+        }
+    }
+
+    public void RemoveConnection(string userId)
+    {
+        if (_activeConnections.ContainsKey(userId))
+        {
+            _activeConnections.Remove(userId);
+        }
+    }
+
+    public async Task EmitToUser(string userId, string eventType, object message)
+    {
+        var messageToSend = new { Type = eventType, Data = message };
+
+        if (_activeConnections.ContainsKey(userId))
+        {
+            var context = _activeConnections[userId];
+            await context.Response.WriteAsync(JsonSerializer.Serialize(messageToSend));
+            await context.Response.Body.FlushAsync();
+        }
+    }
+
+    public async Task EmitToGuild(Task<List<string>> guildUsersTask, object messageToEmit)
+    {
+        List<string> guildUsers = await guildUsersTask; 
+        
+        foreach (var user in guildUsers)
+        {
+            await EmitToUser(user, "guild_event", messageToEmit);
+        }
+    }
+
+}
