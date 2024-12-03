a22213564b489267ec647456852f3f16d751022d
TheLp281 <efekantunc0@gmail.com>
2024-12-04 01:17:40 +0300

    Fix guilduser column error

--- Models/GuildUser.cs
+++ Models/GuildUser.cs
@@ -3,21 +3,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LiventCord.Models
 {
-    [Table("guild_users")]
     public class GuildUser
     {
         [Key]
-        [Column("guild_id", Order = 1)]
+        [Column("guild_id")]
         public required string GuildId { get; set; }
 
         [Key]
-        [Column("user_id", Order = 2)]
+        [Column("user_id")]
         public required string MemberId { get; set; }
 
-        [ForeignKey("guild_id")]
         public virtual required Guild Guild { get; set; }
 
-        [ForeignKey("user_id")]
         public virtual required User User { get; set; }
     }
+
+
 }
